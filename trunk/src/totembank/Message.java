532e5787673926db9fdb7bc5dceddf6a2fcc18ae
Corsu.Fieru@gmail.com <Corsu.Fieru@gmail.com@2761871a-5f5f-7165-6bd4-58423e126bf2>
2010-11-16 16:28:49 +0000

    Add of javadoc comments

--- trunk/src/totembank/Message.java
+++ trunk/src/totembank/Message.java
@@ -1,29 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package totembank;
+
 import java.io.*;
-/**
- *
- * @author Quentin
+
+/** This class defines messages. Messages can be standard message or token or Gateway Messages.
+ * Messages has to be serializable to be sent.
  */
-public class Message implements Serializable{
+public class Message implements Serializable {
+
+    /** Used for serialization*/
     private static final long serialVersionUID = 154634656545456L;
+    /** ID of the ring of origin of the message*/
     int ringID;
-        int seqNum = -1;
-        long tStamp;
+    /** Sequence Number within the local ring */
+    int seqNum = -1;
+    /** Timestamp*/
+    long tStamp;
 
-
-    public int getRingID(){
+    /** @return Id of origin of the message*/
+    public int getRingID() {
         return ringID;
     }
-    public Message(int ring){
+
+    /** Constructor
+    @param ring id of the origin ring*/
+    public Message(int ring) {
         ringID = ring;
         tStamp = System.currentTimeMillis();
 
 
     }
-
 }
--- trunk/src/totembank/Node.java
+++ trunk/src/totembank/Node.java
@@ -1,33 +1,38 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package totembank;
-    import java.net.* ; // MulticastSocket, InetAddress, DatagramPacket,...
-    import java.io.* ; // exceptions comme UnknownHostException, SocketException,...
+
+import java.net.*;
+
 /**
- *
- * @author Quentin
+Defines a Node. There are two points of view for Nodes.
+ * For other Nodes within the rings, Nodes are viewed by their id except in the Bottom Layer where Ip Address and port number are useful to pass the token
+ * For itself, a Process implements nodes but the port number does not represent anything. Indeed a Gateway is a process in several ring, so with several port numbers
  */
 public class Node {
+
+    /** Id of the process*/
     int id;
+    /** IP address*/
     InetAddress ip;
+    /** Port number of the process*/
     int port;
 
-    public Node(InetAddress ip, int port, int id){
+    /** Constructor. */
+    public Node(InetAddress ip, int port, int id) {
         this.id = id;
         this.ip = ip;
         this.port = port;
     }
-    public Node(){
+
+    public Node() {
     }
 
-    public InetAddress getIp(){
+    /** @return IP address*/
+    public InetAddress getIp() {
         return ip;
     }
 
-    public int getPort(){
+    /** @return Return the port number*/
+    public int getPort() {
         return port;
     }
 }
